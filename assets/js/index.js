63f153aa75de8e19fdec4bddb4bf3e59af040aaf
Telcean Marius-Andrei <marius.telcean96@gmail.com>
2022-09-24 13:25:21 +0300

    Add 2 more slideshows and styles to the index page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,59 @@
-var myIndex = 0;
-carousel();
+var indexCartiDeVizita = 0;
+carouselCartiDeVizita();
 
-function carousel() {
+function carouselCartiDeVizita() {
   var i;
-  var x = document.getElementsByClassName("sampleSlides");
+  var x = document.getElementsByClassName("cartiDeVizita");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 9000); // Change image every 2 seconds
+  indexCartiDeVizita++;
+  if (indexCartiDeVizita > x.length) {indexCartiDeVizita = 1}    
+  x[indexCartiDeVizita-1].style.display = "block";  
+  setTimeout(carouselCartiDeVizita, 9000); // Change image every 2 seconds
+
 }
+
+var indexEtichete = 0;
+carouselEtichete();
+var i;
+function carouselEtichete() {
+var y = document.getElementsByClassName("etichete");
+  for (i = 0; i < y.length; i++) {
+    y[i].style.display = "none";  
+  }
+  indexEtichete++;
+  if (indexEtichete > y.length) {indexEtichete = 1}    
+  y[indexEtichete-1].style.display = "block";  
+  setTimeout(carouselEtichete, 9000);
+}
+
+var indexEmbleme = 0;
+carouselEmbleme();
+var i;
+function carouselEmbleme() {
+var y = document.getElementsByClassName("embleme");
+  for (i = 0; i < y.length; i++) {
+    y[i].style.display = "none";  
+  }
+  indexEmbleme++;
+  if (indexEmbleme > y.length) {indexEmbleme = 1}    
+  y[indexEtichete-1].style.display = "block";  
+  setTimeout(carouselEmbleme, 9000);
+}
+
+/* 
+var indexEtichete = 0;
+carouselEtichete();
+var i;
+function carouselEtichete() {
+var y = document.getElementsByClassName("etichete");
+  for (i = 0; i < y.length; i++) {
+    y[i].style.display = "none";  
+  }
+  indexEtichete++;
+  if (indexEtichete > y.length) {indexEtichete = 1}    
+  y[indexEtichete-1].style.display = "block";  
+  setTimeout(carouselEtichete, 9000);
+}
+*/
